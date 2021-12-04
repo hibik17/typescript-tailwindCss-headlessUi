fbad7196f3819da9db1feaeef547e4945cf9dc43
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-12-04 17:58:43 +0900

    typescript with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div className="flex justify-items-center flex-wrap p-4 bg-green-200">
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        1
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        2
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        3
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        4
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        5
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        6
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        7
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        8
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        9
+      </div>
+      <div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">
+        10
+      </div>
+    </div>
+  );
+};
